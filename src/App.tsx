c00320772247f61a9953d482898432f930fd9a59
Haroon Khan <HaroonKhan120704@gmail.com>
2023-02-01 00:01:30 +0000

    added basic jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import React from 'react';
+
 function App() {
-  return <div className='border-2 border-white text-white'>testing</div>;
+  return (
+    <React.Fragment>
+      <h1 className='text-center text-white'>Covid Tracker</h1>
+      <div className='border-2 border-white flex flex-row'>
+        <div className='text-white border-2 border-red-900'>Death</div>
+        <div className='text-white border-2 border-green-900'>Recovered</div>
+        <div className='text-white border-2 border-yellow-400'>Infected</div>
+      </div>
+      <div className='text-white border-2 border-white rounded-md'>Data</div>
+    </React.Fragment>
+  );
 }
 
 export default App;
