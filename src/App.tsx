bec12a2fb3c570f29bc7f94bb6e53cd48e16b482
Haroon Khan <HaroonKhan120704@gmail.com>
2023-02-03 10:08:56 +0000

    added new logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Line } from 'react-chartjs-2';
+import { VictoryPie, VictoryLabel } from 'victory';
 
 interface Data {
   deaths: number;
@@ -9,6 +9,7 @@ interface Data {
 
 const App: React.FC = () => {
   const [total, setTotal] = useState<Data | undefined>();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   fetch('https://disease.sh/v3/covid-19/all')
     .then((res) => {
@@ -20,26 +21,55 @@ const App: React.FC = () => {
 
   return (
     <>
-      <h1 className='text-center text-white'>Today's Covid Cases</h1>
-      <form action=''>
-        <input type='text' placeholder='Enter Country'></input>
-      </form>
-      <div className='border-2 border-white flex flex-row'>
-        <div className='text-white border-2 border-red-900'>
-          <p>Daily Total Death</p>
-          {total?.deaths}
-        </div>
-        <div className='text-white border-2 border-green-900'>
-          <p>Daily Total Recovered</p>
-          {total?.recovered}
-        </div>
-        <div className='text-white border-2 border-yellow-400'>
-          <p>Daily Total Cases</p>
-          {total?.cases}
-        </div>
-      </div>
-      <div className='text-white border-2 border-white rounded-md'>
-        GRAPH GOES HERE
+      <h1 className='text-center text-white'>Total Covid Cases</h1>
+      <div>
+        <form action=''>
+          <input
+            type='text'
+            placeholder='Enter Country'
+            className='border-2 border-white bg-transparent text-white placeholder-white rounded-md text-center p-3 font-bold m-auto flex flex-col'
+            onClick={() => setIsModalOpen(true)}
+          ></input>
+        </form>
+        {isModalOpen && (
+          <div className='modal'>
+            <div className='text-center border-2 border-white'>
+              <p className='text-white'>Countries</p>
+
+              <div className='border-2 border-white flex flex-row'>
+                <div className='text-white border-2 border-red-900'>
+                  <p>Daily Total Death</p>
+                  {total?.deaths}
+                </div>
+                <div className='text-white border-2 border-green-900'>
+                  <p>Daily Total Recovered</p>
+                  {total?.recovered}
+                </div>
+                <div className='text-white border-2 border-yellow-400'>
+                  <p>Daily Total Cases</p>
+                  {total?.cases}
+                </div>
+              </div>
+              <div className='text-white border-2 border-white rounded-md h-96'>
+                <VictoryPie
+                  data={[
+                    { x: 'Deaths', y: total?.deaths },
+                    { x: 'Cases', y: total?.cases },
+                    { x: 'Recovered', y: total?.recovered },
+                  ]}
+                  colorScale={['red', 'yellow', 'green']}
+                  labelComponent={<VictoryLabel style={{ fill: 'white' }} />}
+                />
+              </div>
+              <button
+                onClick={() => setIsModalOpen(false)}
+                className='text-red-800'
+              >
+                X
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
