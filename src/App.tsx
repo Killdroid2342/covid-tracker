669df5d9f92103b198bacda706cd7eff1eb76b71
Haroon Khan <HaroonKhan120704@gmail.com>
2023-02-05 00:12:16 +0000

    made data dynamic with click

--- src/App.tsx
+++ src/App.tsx
@@ -11,42 +11,71 @@ const App: React.FC = () => {
   const [total, setTotal] = useState<Data | undefined>();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  fetch('https://disease.sh/v3/covid-19/all')
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      setTotal(data);
-    });
+  const fetchData = (country: string) => {
+    let url = `https://disease.sh/v3/covid-19/all`;
+
+    if (country === 'uk') {
+      url = `https://disease.sh/v3/covid-19/countries/gb`;
+    } else if (country === 'usa') {
+      url = `https://disease.sh/v3/covid-19/countries/us`;
+    }
+
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setTotal(data);
+      });
+  };
 
   return (
     <>
       <h1 className='text-center text-white'>Total Covid Cases</h1>
       <div>
-        <form action=''>
-          <input
-            type='text'
-            placeholder='Enter Country'
-            className='border-2 border-white bg-transparent text-white placeholder-white rounded-md text-center p-3 font-bold m-auto flex flex-col'
-            onClick={() => setIsModalOpen(true)}
-          ></input>
-        </form>
+        <div className='flex flex-row'>
+          <p
+            className='cursor-pointer border-2 border-white bg-transparent text-white placeholder-white rounded-md text-center p-3 font-bold m-auto flex flex-col'
+            onClick={() => {
+              setIsModalOpen(true);
+              fetchData('worldwide');
+            }}
+          >
+            Get WorldWide cases
+          </p>
+          <p
+            className='cursor-pointer border-2 border-white bg-transparent text-white placeholder-white rounded-md text-center p-3 font-bold m-auto flex flex-col'
+            onClick={() => {
+              setIsModalOpen(true);
+              fetchData('uk');
+            }}
+          >
+            UK Cases
+          </p>
+          <p
+            className='cursor-pointer border-2 border-white bg-transparent text-white placeholder-white rounded-md text-center p-3 font-bold m-auto flex flex-col'
+            onClick={() => {
+              setIsModalOpen(true);
+              fetchData('usa');
+            }}
+          >
+            USA Cases
+          </p>
+        </div>
         {isModalOpen && (
           <div className='modal'>
             <div className='text-center border-2 border-white'>
-              <p className='text-white'>Countries</p>
-
               <div className='border-2 border-white flex flex-row'>
                 <div className='text-white border-2 border-red-900'>
-                  <p>Daily Total Death</p>
+                  <p>Total Death</p>
                   {total?.deaths}
                 </div>
                 <div className='text-white border-2 border-green-900'>
-                  <p>Daily Total Recovered</p>
+                  <p>Total Recovered</p>
                   {total?.recovered}
                 </div>
                 <div className='text-white border-2 border-yellow-400'>
-                  <p>Daily Total Cases</p>
+                  <p>Total Cases</p>
                   {total?.cases}
                 </div>
               </div>
