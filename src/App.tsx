e29bc1a233badd26bdd848f276fc76451f628dc9
Haroon Khan <HaroonKhan120704@gmail.com>
2023-02-10 00:15:22 +0000

    got all countries

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import Modal from './components/Modal/Modal';
 import Table from './components/Table/Table';
 
 interface Data {
+  continent: number;
+  critical: number;
+  todayRecovered: number;
+  todayDeaths: number;
+  todayCases: number;
+  tests: number;
   deaths: number;
   recovered: number;
   cases: number;
@@ -14,42 +20,31 @@ export interface TCountryData {
   data?: Data;
 }
 
-const countries: TCountryData[] = [
-  { name: 'WorldWide', code: 'all' },
-  { name: 'UK', code: 'gb' },
-  { name: 'USA', code: 'us' },
-];
-
 const App: React.FC = () => {
-  const [countryData, setCountryData] = useState<TCountryData[]>(countries);
+  const [countryData, setCountryData] = useState<TCountryData[]>([]);
   const [selectedData, setSelectedData] = useState<TCountryData | undefined>();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const fetchData = async (country: TCountryData[]) => {
+  const fetchData = async () => {
     let tempList: TCountryData[] = [];
-    countries.forEach(async (country, i) => {
-      let url = `https://disease.sh/v3/covid-19/countries/${country.name}`;
-      if (country.code == 'all') {
-        url = `https://disease.sh/v3/covid-19/${country.code}`;
-      }
-      const data = await fetch(url)
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          country.data = data;
-          return country;
+    await fetch('https://disease.sh/v3/covid-19/countries')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((country: any) => {
+          tempList.push({
+            name: country.country,
+            code: country.countryInfo.iso2,
+            data: country,
+          });
         });
-
-      tempList.push(data);
-      if (i == countries.length) {
         setCountryData(tempList);
-      }
-    });
+      });
   };
 
   useEffect(() => {
-    fetchData(countries);
+    fetchData();
   }, []);
 
   return (
