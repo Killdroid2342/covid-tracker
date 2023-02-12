f29fca997504a111912b0d2f340bc211c030cfcf
Haroon Khan <HaroonKhan120704@gmail.com>
2023-02-12 00:58:28 +0000

    removed all the warnings

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -13,60 +13,60 @@ const Table = ({ setIsModalOpen, countryData, setSelectedData }: Props) => {
     setIsModalOpen(true);
   };
 
-  console.log('COUNTRY DATA', countryData);
   return (
     <div>
       <table>
-        <tbody className='relative'>
-          <div className='border border-white overflow-auto h-[80vw]'>
-            <th className='text-white'>Countries</th>
-            <th className='text-white'>Deaths</th>
-            <th className='text-white'>Recovered</th>
-            <th className='text-white'>Cases</th>
-            <th className='text-white'>Tests</th>
-            <th className='text-white'>Cases Today</th>
-            <th className='text-white'>Deaths Today</th>
-            <th className='text-white'>Recovered Today</th>
-            <th className='text-white'>Critical</th>
-            <th className='text-white'>Continent</th>
-            {countryData.map((data, i) => (
-              <tr key={i}>
-                <td
-                  className='border border-green-900 cursor-pointer text-white'
-                  onClick={() => handleRowClick(data)}
-                >
-                  {data.name}
-                </td>
-                <td className='border border-green-900 text-white'>
-                  {data.data && data.data.deaths}
-                </td>
-                <td className='border border-green-900 text-white'>
-                  {data.data && data.data.recovered}
-                </td>
-                <td className='border border-green-900 text-white'>
-                  {data.data && data.data.cases}
-                </td>
-                <td className='border border-green-900 text-white'>
-                  {data.data && data.data.tests}
-                </td>
-                <td className='border border-green-900 text-white'>
-                  {data.data && data.data.todayCases}
-                </td>
-                <td className='border border-green-900 text-white'>
-                  {data.data && data.data.todayDeaths}
-                </td>
-                <td className='border border-green-900 text-white'>
-                  {data.data && data.data.todayRecovered}
-                </td>
-                <td className='border border-green-900 text-white'>
-                  {data.data && data.data.critical}
-                </td>
-                <td className='border border-green-900 text-white'>
-                  {data.data && data.data.continent}
-                </td>
-              </tr>
-            ))}
-          </div>
+        <thead className='text-white'>
+          <tr className='border border-white overflow-auto'>
+            <td>Countries</td>
+            <td>Deaths</td>
+            <td>Recovered</td>
+            <td>Cases</td>
+            <td>Tests</td>
+            <td>Cases Today</td>
+            <td>Deaths Today</td>
+            <td>Recovered Today</td>
+            <td>Critical</td>
+            <td>Continent</td>
+          </tr>
+        </thead>
+        <tbody>
+          {countryData.map((data, i) => (
+            <tr
+              key={i}
+              className='border border-green-900 cursor-pointer text-white'
+              onClick={() => handleRowClick(data)}
+            >
+              <td>{data.name}</td>
+              <td className='border border-green-900 text-white'>
+                {data.data && data.data.deaths}
+              </td>
+              <td className='border border-green-900 text-white'>
+                {data.data && data.data.recovered}
+              </td>
+              <td className='border border-green-900 text-white'>
+                {data.data && data.data.cases}
+              </td>
+              <td className='border border-green-900 text-white'>
+                {data.data && data.data.tests}
+              </td>
+              <td className='border border-green-900 text-white'>
+                {data.data && data.data.todayCases}
+              </td>
+              <td className='border border-green-900 text-white'>
+                {data.data && data.data.todayDeaths}
+              </td>
+              <td className='border border-green-900 text-white'>
+                {data.data && data.data.todayRecovered}
+              </td>
+              <td className='border border-green-900 text-white'>
+                {data.data && data.data.critical}
+              </td>
+              <td className='border border-green-900 text-white'>
+                {data.data && data.data.continent}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
