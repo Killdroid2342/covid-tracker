b1d69727deadbd41e921e952420a02d0759d9a58
Haroon Khan <HaroonKhan120704@gmail.com>
2023-02-12 00:58:55 +0000

    added better styles to modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -10,32 +10,34 @@ interface Props {
 const Modal = ({ isModalOpen, setIsModalOpen, selectedData }: Props) => {
   if (!isModalOpen) return null;
   return (
-    <div className='absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-[540px] bg-slate-500 p-4 border-2 border-white rounded-lg'>
-      <div className='text-center '>
-        <p className='text-white font-bold text-2xl'>
-          {selectedData ? selectedData.name : ''}
-        </p>
-        <div className=' flex flex-row justify-around py-2'>
-          <div className='text-white border-2 bg-red-900 rounded-lg px-2'>
-            <p className='font-bold'>Total Death</p>
-            {selectedData ? selectedData.data.deaths : ''}
+    <div className='backdrop-blur-sm w-screen h-screen fixed top-0 left-0 flex justify-center items-center'>
+      <div className=' h-fit w-[540px] bg-slate-500 p-4 border-2 border-white rounded-lg relative'>
+        <div className='text-center '>
+          <p className='text-white font-bold text-2xl'>
+            {selectedData ? selectedData.name : ''}
+          </p>
+          <div className=' flex flex-row justify-around py-2'>
+            <div className='text-white border-2 bg-red-900 rounded-lg px-2'>
+              <p className='font-bold'>Total Death</p>
+              {selectedData ? selectedData.data.deaths : ''}
+            </div>
+            <div className='text-white border-2 bg-green-900 rounded-lg px-2'>
+              <p className='font-bold'>Total Recovered</p>
+              {selectedData ? selectedData.data.recovered : ''}
+            </div>
+            <div className='text-white border-2 bg-yellow-400 rounded-lg px-2'>
+              <p className='font-bold'>Total Cases</p>
+              {selectedData ? selectedData.data.cases : ''}
+            </div>
           </div>
-          <div className='text-white border-2 bg-green-900 rounded-lg px-2'>
-            <p className='font-bold'>Total Recovered</p>
-            {selectedData ? selectedData.data.recovered : ''}
-          </div>
-          <div className='text-white border-2 bg-yellow-400 rounded-lg px-2'>
-            <p className='font-bold'>Total Cases</p>
-            {selectedData ? selectedData.data.cases : ''}
-          </div>
+          <PieChart total={selectedData && selectedData.data} />
+          <button
+            onClick={() => setIsModalOpen(false)}
+            className='text-black font-extrabold text-2xl absolute top-2 right-2'
+          >
+            X
+          </button>
         </div>
-        <PieChart total={selectedData && selectedData.data} />
-        <button
-          onClick={() => setIsModalOpen(false)}
-          className='text-black font-extrabold text-2xl absolute top-2 right-2'
-        >
-          X
-        </button>
       </div>
     </div>
   );
