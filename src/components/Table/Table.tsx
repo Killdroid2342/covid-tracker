823681e7eb53a7288ec25a324124e9ce8889dc31
Haroon Khan <HaroonKhan120704@gmail.com>
2023-02-13 02:33:25 +0000

    simple styling

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -14,55 +14,75 @@ const Table = ({ setIsModalOpen, countryData, setSelectedData }: Props) => {
   };
 
   return (
-    <div>
-      <table>
-        <thead className='text-white'>
-          <tr className='border border-white overflow-auto'>
-            <td>Countries</td>
-            <td>Deaths</td>
-            <td>Recovered</td>
-            <td>Cases</td>
-            <td>Tests</td>
-            <td>Cases Today</td>
-            <td>Deaths Today</td>
-            <td>Recovered Today</td>
-            <td>Critical</td>
-            <td>Continent</td>
+    <div className='flex flex-col justify-center items-center '>
+      <table className='border-2 border-neutral-400 text-center text-neutral-400 p-5'>
+        <thead>
+          <tr>
+            <td className='border-2 border-neutral-400 font-bold text-center bg-white text-neutral-500 p-5'>
+              Countries
+            </td>
+            <td className='border-2 border-neutral-400 font-bold text-center bg-white text-neutral-500 p-5'>
+              Deaths
+            </td>
+            <td className='border-2 border-neutral-400 font-bold text-center bg-white text-neutral-500 p-5'>
+              Recovered
+            </td>
+            <td className='border-2 border-neutral-400 font-bold text-center bg-white text-neutral-500 p-5'>
+              Cases
+            </td>
+            <td className='border-2 border-neutral-400 font-bold text-center bg-white text-neutral-500 p-5'>
+              Tests
+            </td>
+            <td className='border-2 border-neutral-400 font-bold text-center bg-white text-neutral-500 p-5'>
+              Cases Today
+            </td>
+            <td className='border-2 border-neutral-400 font-bold text-center bg-white text-neutral-500 p-5'>
+              Deaths Today
+            </td>
+            <td className='border-2 border-neutral-400 font-bold text-center bg-white text-neutral-500 p-5'>
+              Recovered Today
+            </td>
+            <td className='border-2 border-neutral-400 font-bold text-center bg-white text-neutral-500 p-5'>
+              Critical
+            </td>
+            <td className='border-2 border-neutral-400 font-bold text-center bg-white text-neutral-500 p-5'>
+              Continent
+            </td>
           </tr>
         </thead>
         <tbody>
           {countryData.map((data, i) => (
             <tr
               key={i}
-              className='border border-green-900 cursor-pointer text-white'
+              className='border-2 border-neutral-400 cursor-pointer text-white p-2'
               onClick={() => handleRowClick(data)}
             >
               <td>{data.name}</td>
-              <td className='border border-green-900 text-white'>
+              <td className='border-2 border-neutral-400 text-center text-white p-2'>
                 {data.data && data.data.deaths}
               </td>
-              <td className='border border-green-900 text-white'>
+              <td className='border-2 border-neutral-400 text-center text-white p-2'>
                 {data.data && data.data.recovered}
               </td>
-              <td className='border border-green-900 text-white'>
+              <td className='border-2 border-neutral-400 text-center text-white p-2'>
                 {data.data && data.data.cases}
               </td>
-              <td className='border border-green-900 text-white'>
+              <td className='border-2 border-neutral-400 text-center text-white p-2'>
                 {data.data && data.data.tests}
               </td>
-              <td className='border border-green-900 text-white'>
+              <td className='border-2 border-neutral-400 text-center text-white p-2'>
                 {data.data && data.data.todayCases}
               </td>
-              <td className='border border-green-900 text-white'>
+              <td className='border-2 border-neutral-400 text-center text-white p-2'>
                 {data.data && data.data.todayDeaths}
               </td>
-              <td className='border border-green-900 text-white'>
+              <td className='border-2 border-neutral-400 text-center text-white p-2'>
                 {data.data && data.data.todayRecovered}
               </td>
-              <td className='border border-green-900 text-white'>
+              <td className='border-2 border-neutral-400 text-center text-white p-2'>
                 {data.data && data.data.critical}
               </td>
-              <td className='border border-green-900 text-white'>
+              <td className='border-2 border-neutral-400 text-center text-white p-2'>
                 {data.data && data.data.continent}
               </td>
             </tr>
